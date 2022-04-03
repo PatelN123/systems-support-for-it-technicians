58655e87761f2bafa07479cabf599c49bd9c9603
Ayaz Zafar <ayaziqbalzafar@gmail.com>
2022-04-03 19:24:34 +0100

    Create about.tsx

---
+++ pages/about.tsx
@@ -0,0 +1,14 @@
+import Navbar from '../src/components/navbar'
+import Footer from '../src/components/footer'
+ 
+  function IndexPage() {
+    return (
+      <>
+        <Navbar />
+
+        <Footer />
+      </>
+    )
+  }
+ 
+  export default IndexPage
